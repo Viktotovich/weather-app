c7f27283525c82fe8d316107f4421c92a0ce97f6
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-10-04 21:27:16 +0400

    "Created the HTML layout from README.MD with minor adjustments"

--- src/components/dom-manager.js
+++ src/components/dom-manager.js
@@ -1,1 +1,12 @@
+/* eslint-disable */
+//import { requestsController } from "./components/requests-manager";
+//requestsController.createRequest("dubai");
+
 //import capitals.json to aid with the search box
+import capitals from "./json/capitals.json";
+
+// This imports everything, and does not export anything except the function to initiate
+
+const domController = {
+  initiateDom: function () {},
+};
---
+++ src/components/icon-manager.js
@@ -0,0 +1,1 @@
+//Take the weather status, and based on the status either use GIFY or append images that correlate with the data.
